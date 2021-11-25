adb702cd23520ab37fe824a542da9c01ac3fba75
fs0c13ty-iwnl <50542897+fs0c13ty-iwnl@users.noreply.github.com>
2021-11-25 17:06:21 +0800

    Update SocketService.java

--- SocketService.java
+++ SocketService.java
@@ -1,9 +1,5 @@
 //*********************************************************//
 //            Distributed Systems  Assignment 3            //
-//    Filename     -   SocketServer.java                   //
-//    Author       -   Songzhe Li                          //
-//    Student ID   -   a1767109                            //
-//    E-mail       -   a1767109@student.adelaide.edu.au    //
 //*********************************************************//
 import java.io.InputStream;
 import java.io.ObjectInputStream;
